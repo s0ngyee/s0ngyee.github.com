fe54b6b806379e6bef06916ef9b5c3294d22714e
s0ngyee <songsong10@gmail.com>
2014-02-14 04:31:06 +0800

    modify slide1

--- shugetsu/js/main.js
+++ shugetsu/js/main.js
@@ -1,20 +1,28 @@
 $(function(){
-    $("#demo").typewriter({
-        delay: 500,
-        random: true,
-        randomMax: 200
-    });
 
-    $('.love>div>div>div').each(function(id){
-        $(this).css({
+    //slide cover
+    $('.love > div > div > div').each(function(){
+        var el = $(this);
+        el.css({
             position: 'relative',
             top: '-200px',
             opacity: 0
         });
         var wait = Math.floor((Math.random()*3000)+1);
-        $(this).delay(wait).animate({
-            top: '400px',
+        el.delay(wait).animate({
+            top: '200px',
             opacity: 1
         },1000);
+    });
+
+    $('#cover').click(function(){
+        console.log('#### change ####');
     })
+
+
+    $("#demo").typewriter({
+        delay: 500,
+        random: true,
+        randomMax: 200
+    });
 })
